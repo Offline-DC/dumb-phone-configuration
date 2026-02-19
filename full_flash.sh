7051cdbe255c6cca5f628cd2aea03613c0424138
Jack Nugent <31449177+jacknugent@users.noreply.github.com>
2026-02-19 15:56:54 -0500

    Add largest

--- full_flash.sh
+++ full_flash.sh
@@ -40,7 +40,8 @@ if [[ "$VERSION" == "pete" ]]; then
   echo "1) Go through onboarding"
   echo "2) Go to Settings -> Phone Settings -> Key shortcuts, and make LEFT shortcut key WhatsApp, and RIGHT shortcut key Uber"
   echo "3) Go to Settings -> Display -> Menu layout, and select list"
-  echo "4) Final triple check that keys open both apps as expected"
+  echo "4) Go to Settings -> Display -> font size, and select Largest"
+  echo "5) Final triple check that keys open both apps as expected"
   exit 0
 fi
 
